0d90558311f1888168a6e8ba57e67c44af360eed
fatih efkan <mfefkan@hotmail.com>
2023-10-18 15:25:47 +0300

    Renderer2 sınıfı incelendi ve örnekle kullanıldı. Renderer2 sayesinde html ile ilgili yaptığımız manipulasyonların bütün platformlarda sorunsuz çalışmasını sağlıyoruz. Bu sınıf bir çeşit entegrasyon yapıyor diyebiliriz. Angular'ın içinde hazır gelmektedir...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  template: `
+  <h1 #h>App Component</h1>
+  `,
 })
 export class AppComponent {
-  title = 'Renderer2';
+  @ViewChild("h",{static:true}) _h:ElementRef;
+
+  constructor(private renderer : Renderer2) {}
+  ngOnInit() {
+    this.renderer.setStyle(this._h.nativeElement,"color","red")
+  }
+
 }
+
+//Renderer2 sınıfı crossplatform çalışma konusunda işimizi garantiye almamızı sağlayan bir sınıftır. 
